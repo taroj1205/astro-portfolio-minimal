9631fefcadc556779f237667ee8c6d47d93874a9
Shintaro Jokagi <taroj1205@gmail.com>
2025-03-09 16:36:41 +1300

    chore: update config to remove inline asset limit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    build: {
-      assetsInlineLimit: 512,
-    },
-  },
-});
+export default defineConfig({});
