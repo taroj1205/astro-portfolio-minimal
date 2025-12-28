e5fa96635292afd9601e3c24cfc46a00544abccd
Shintaro Jokagi <taroj1205@gmail.com>
2025-12-28 22:29:53 +0900

    chore(semantics): improve overall html semantics of portfolio and use unpic image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
 import mdx from '@astrojs/mdx'
+import { imageService } from '@unpic/astro/service'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+  image: {
+    service: imageService(),
+  },
   devToolbar: {
     enabled: false,
   },
   scopedStyleStrategy: 'class',
+  build: {
+    inlineStylesheets: 'always',
+  },
 })
