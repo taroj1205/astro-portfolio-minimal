e6ea9451838a8ba91b5c79587e08a2dca7f5d52a
Shintaro Jokagi <taroj1205@gmail.com>
2025-12-28 22:56:03 +0900

    chore(astro,image): remove unpic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import mdx from '@astrojs/mdx'
-import { imageService } from '@unpic/astro/service'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  image: {
-    service: imageService(),
-  },
   devToolbar: {
     enabled: false,
   },
